cf3c009ee9b68523b5ebf48cbf2e729b0f3d4318
MrZloHex <zlo.alex.it@gmail.com>
2021-05-05 16:09:16 +0300

    grid

--- main.cpp
+++ main.cpp
@@ -1,19 +1,19 @@
 #include <SFML/Graphics.hpp>
 
-
 using namespace sf;
 
 int main()
 {
 	// making window	
-	RenderWindow window(VideoMode(1920, 1080), "sseabottle");
+	RenderWindow window(VideoMode(575, 300), "sseabottle");
 
-	Texture ship;
-	ship.loadFromFile("./img/ship.png");
+	int common_offset = 25, my_offset = 300, enemy_offset = 25;
+	int mygrid[10][10];
+	int engrid[10][10];
 
-	Sprite sh(ship);
+	
 
-
+ 
 	// life cycle
 	while (window.isOpen())
 	{
@@ -21,23 +21,42 @@ int main()
 		//Vector2i pos = Mouse::getPosition(window) - Vector2i(offset);
 
 		// polling events
-        	Event event;
-
-        	while (window.pollEvent(event))
-        	{
+        Event event;
+        while (window.pollEvent(event))
+        {
 			// button *close* was pressed manual closing window
-            		if (event.type == Event::Closed) 
+            if (event.type == Event::Closed) 
 			{
-                		window.close();
+				window.close();
 			}
-        	}
+        }
 		
-		// refresh part
-        	window.clear(Color::White);
-		window.draw(sh);
-        	window.display();
-    	}
+        for (int k = 0; k < 2; k++)
+        {
+        	for (int i = 0; i < 10; i++)
+        	{
+        		for (int j = 0; j < 10; j++)
+        		{
+        			RectangleShape square(Vector2f(23.0f, 23.0f));
+					square.setFillColor(Color(0, 255, 255));
+					if (k == 0)
+					{
+        				square.setPosition(i*common_offset+enemy_offset, j*common_offset+common_offset);
+        			}
+        			else
+   					{
+   						square.setPosition(i*common_offset+my_offset, j*common_offset+common_offset);
+   					}
+        			window.draw(square);
+        		}
+        	}
+        }
 
-    	return 0;
+		// refresh part
+       	//window.clear(Color::White);
+		window.display();
+	}
+
+	return 0;
 }
 
