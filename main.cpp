0805600b075462e57affc4c11721924f332d3a00
MrZloHex <zlo.alex.it@gmail.com>
2021-05-05 18:12:40 +0300

    first shooylike mvmt

--- main.cpp
+++ main.cpp
@@ -8,10 +8,17 @@ int main()
 	RenderWindow window(VideoMode(575, 300), "sseabottle");
 
 	int common_offset = 25, my_offset = 300, enemy_offset = 25;
-	int mygrid[10][10];
-	int engrid[10][10];
+	int my_grid[10][10];
+	int enemy_grid[10][10];
 
-	
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			my_grid[i][j] = 0;
+			enemy_grid[i][j] = 0;
+		}
+	}
 
  
 	// life cycle
@@ -19,6 +26,11 @@ int main()
 	{
 		// getting mouse position
 		//Vector2i pos = Mouse::getPosition(window) - Vector2i(offset);
+
+		Vector2i pos = Mouse::getPosition(window);
+        int x = pos.x/common_offset;
+        int y = pos.y/common_offset;
+
 
 		// polling events
         Event event;
@@ -29,6 +41,9 @@ int main()
 			{
 				window.close();
 			}
+
+			if (event.type == Event::MouseButtonPressed)
+              if (event.key.code == Mouse::Left) my_grid[x][y]=1;
         }
 		
         for (int k = 0; k < 2; k++)
@@ -38,7 +53,14 @@ int main()
         		for (int j = 0; j < 10; j++)
         		{
         			RectangleShape square(Vector2f(23.0f, 23.0f));
-					square.setFillColor(Color(0x00, 0xFF, 0xFF));
+        			if (my_grid[i+1][j+1] == 0)
+        			{
+						square.setFillColor(Color(0x00, 0xFF, 0xFF));
+					} else {
+						square.setFillColor(Color(0xFF, 0x00, 0x00));
+					}
+
+
 					if (k == 0)
 					{
         				square.setPosition(i*common_offset+enemy_offset, j*common_offset+common_offset);
