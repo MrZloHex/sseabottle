edaa767259b62c08538eb0bb0b4a6737d60c1ffc
MrZloHex <zlo.alex.it@gmail.com>
2021-05-05 14:38:41 +0300

    first sprite

--- main.cpp
+++ main.cpp
@@ -6,26 +6,35 @@ using namespace sf;
 int main()
 {
 	// making window	
-	RenderWindow window(VideoMode(900, 400), "sseabottle");
-	CircleShape shape(100.f);
-	shape.setFillColor(Color::Green);
+	RenderWindow window(VideoMode(1920, 1080), "sseabottle");
+
+	Texture ship;
+	ship.loadFromFile("./img/ship.png");
+
+	Sprite sh(ship);
+
 
 	// life cycle
 	while (window.isOpen())
 	{
+		// getting mouse position
+		//Vector2i pos = Mouse::getPosition(window) - Vector2i(offset);
+
 		// polling events
         	Event event;
 
         	while (window.pollEvent(event))
         	{
 			// button *close* was pressed manual closing window
-            		if (event.type == Event::Closed)
+            		if (event.type == Event::Closed) 
+			{
                 		window.close();
+			}
         	}
 		
 		// refresh part
-        	window.clear();
-        	window.draw(shape);
+        	window.clear(Color::White);
+		window.draw(sh);
         	window.display();
     	}
 
