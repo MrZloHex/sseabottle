865582c67a8fbbf7b6876a3389ce7b550e6b3187
MrZloHex <zlo.alex.it@gmail.com>
2021-05-06 12:28:17 +0300

    refactor of code

--- main.cpp
+++ main.cpp
@@ -5,6 +5,110 @@ using namespace std;
 using namespace sf;
 
 
+const bool dev = false;  // 0 - for normal mode; 1 - for dev mode
+
+
+int * drawing_squares(int type, int x, int y, int my_grid[10][10], int enemy_grid[10][10], int my_enemy_grid[10][10]) {
+	// drawing enemy grid go me
+
+        /* 									 R     G     B
+        0 - water							0x00, 0xFF, 0xFF
+        1 - water, which was shooted		0x00, 0x80, 0x80
+        2 - ship							0x3F, 0x3F, 0x3F
+        3 - ship, which was shooted			0xFF, 0x80, 0x00
+    	. - nothing or trouble				0x00, 0xFF, 0x00
+    */
+    // set colour
+	static int colour[3];
+    if (type == 0) {
+    	if (!dev) {
+    		if (my_enemy_grid[x][y] == 0) {
+        		colour[0] = 0x00;
+        		colour[1] = 0xFF;
+        		colour[2] = 0xFF;
+
+        	}
+        	else if (my_enemy_grid[x][y] == 1){
+       			colour[0] = 0x00;
+        		colour[1] = 0x80;
+        		colour[2] = 0x80;
+        	}
+      		else if (my_enemy_grid[x][y] == 2){
+       			colour[0] = 0x3F;
+        		colour[1] = 0x3F;
+        		colour[2] = 0x3F;
+       		}
+        	else if (my_enemy_grid[x][y] == 3){
+        		colour[0] = 0xFF;
+        		colour[1] = 0x80;
+        		colour[2] = 0x00;
+        	}
+        	else {
+        		colour[0] = 0x00;
+        		colour[1] = 0xFF;
+        		colour[2] = 0x00;
+        	}
+    	}
+    	else if (dev) {
+    		if (enemy_grid[x][y] == 0) {
+        		colour[0] = 0x00;
+        		colour[1] = 0xFF;
+        		colour[2] = 0xFF;
+
+        	}
+        	else if (enemy_grid[x][y] == 1){
+       			colour[0] = 0x00;
+        		colour[1] = 0x80;
+        		colour[2] = 0x80;
+        	}
+      		else if (enemy_grid[x][y] == 2){
+       			colour[0] = 0x3F;
+        		colour[1] = 0x3F;
+        		colour[2] = 0x3F;
+       		}
+        	else if (enemy_grid[x][y] == 3){
+        		colour[0] = 0xFF;
+        		colour[1] = 0x80;
+        		colour[2] = 0x00;
+        	}
+        	else {
+        		colour[0] = 0x00;
+        		colour[1] = 0xFF;
+        		colour[2] = 0x00;
+        	}
+    	}
+    }
+    else {
+    	if (my_grid[x][y] == 0) {
+        	colour[0] = 0x00;
+        	colour[1] = 0xFF;
+       		colour[2] = 0xFF;
+        }
+        else if (my_grid[x][y] == 1){
+       		colour[0] = 0x00;
+        	colour[1] = 0x80;
+        	colour[2] = 0x80;
+       	}
+      	else if (my_grid[x][y] == 2){
+      		colour[0] = 0x3F;
+       		colour[1] = 0x3F;
+        	colour[2] = 0x3F;
+       	}
+        else if (my_grid[x][y] == 3){
+        	colour[0] = 0xFF;
+        	colour[1] = 0x80;
+        	colour[2] = 0x00;
+        }
+        else {
+        	colour[0] = 0x00;
+        	colour[1] = 0xFF;
+        	colour[2] = 0x00;
+        }
+    }
+    return colour;
+}
+
+
 void generate_enemy_field(int field[0xA][0xA]) {
 	int amount = 10;
 	for (int x = 0; x < 10; x++) {
@@ -52,7 +156,6 @@ void generate_enemy_field(int field[0xA][0xA]) {
 	}
 }
 
-
 void make_field(int field[10][10]) {
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
@@ -60,7 +163,6 @@ void make_field(int field[10][10]) {
 		}
 	}
 }
-
 
 
 int main() {
@@ -79,21 +181,6 @@ int main() {
 
 	generate_enemy_field(enemy_grid);
 
-	/*for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			if ((rand()%4) == 0) {
-				enemy_grid[i][j] = 2;
-			} 
-			else {
-				enemy_grid[i][j] = 0;
-			}
-			my_grid[i][j] = 0;
-			my_enemy_grid[i][j] = 0;
-
-		}
-	}*/
-
- 
 	// life cycle
 	while (window.isOpen()) {
 		Vector2i pos = Mouse::getPosition(window);
@@ -108,7 +195,6 @@ int main() {
             if (event.type == Event::Closed) {
 				window.close();
 			}
-
 			if (event.type == Event::MouseButtonPressed) {
             	if (event.key.code == Mouse::Left) {
               		/*cout << "x " << x << endl;
@@ -125,78 +211,36 @@ int main() {
               				//cout << "x " << x << " y " << y << " " << my_enemy_grid[x][y] << endl;
               			}
               		}
-              		//my_grid[x][y]=1;
               	}
 			}
         }
 		
         //window.clear();
 
-        for (int k = 0; k < 2; k++) {
-        	for (int i = 0; i < 10; i++) {
-        		for (int j = 0; j < 10; j++) {
+        for (int type = 0; type < 2; type++) {
+        	for (int x = 0; x < 10; x++) {
+        		for (int y = 0; y < 10; y++) {
         			RectangleShape square(Vector2f(23.0f, 23.0f));
 
 
-        			if (k == 0) {
+        			if (type == 0) {
         				// drawing enemy grid go me
-
-        				/* 									 R     G     B
-        				   0 - water						0x00, 0xFF, 0xFF
-        				   1 - water, which was shooted		0x00, 0x80, 0x80
-        				   2 - ship							0xF0, 0xF0, 0xF0
-        				   3 - ship, which was shooted		0xFF, 0x80, 0x00
-        				   . - nothing or trouble			0x00, 0xFF, 0x00
-        				*/
-        				// set colour
-        				if (enemy_grid[i][j] == 0) {
-        					square.setFillColor(Color(0x00, 0xFF, 0xFF));
-        				}
-        				else if (enemy_grid[i][j] == 1){
-        					square.setFillColor(Color(0x00, 0x80, 0x80));
-        				}
-        				else if (enemy_grid[i][j] == 2){
-        					square.setFillColor(Color(0xF0, 0xF0, 0xF0));
-        				}
-        				else if (enemy_grid[i][j] == 3){
-        					square.setFillColor(Color(0xFF, 0x80, 0x00));
-        				}
-        				else {
-        					square.setFillColor(Color(0x00, 0xFF, 0x00));
-        				}
-
-
+        				int *colour = drawing_squares(type, x, y, my_grid, enemy_grid, my_enemy_grid);
+        				square.setFillColor(Color(colour[0], colour[1], colour[2]));
         				// placing them and drawing
-        				square.setPosition((i * common_offset) + enemy_offset, (j * common_offset) + common_offset);
+        				square.setPosition((x * common_offset) + enemy_offset, (y * common_offset) + common_offset);
         			}
         			else {
         				// set colour
-        				if (my_grid[i][j] == 0) {
-        					square.setFillColor(Color(0x00, 0xFF, 0xFF));
-        				}
-        				else if (my_grid[i][j] == 1){
-        					square.setFillColor(Color(0x00, 0xF0, 0xF0));
-        				}
-        				else if (my_grid[i][j] == 2){
-        					square.setFillColor(Color(0xF0, 0xF0, 0xF0));
-        				}
-        				else if (my_grid[i][j] == 3){
-        					square.setFillColor(Color(0xFF, 0xF0, 0x00));
-        				}
-        				else {
-        					square.setFillColor(Color(0x00, 0xFF, 0x00));
-        				}
-
-
+        				int *colour = drawing_squares(type, x, y, my_grid, enemy_grid, my_enemy_grid);
+        				square.setFillColor(Color(colour[0], colour[1], colour[2]));
         				// placing them and drawing
-        				square.setPosition((i * common_offset) + my_offset, (j * common_offset) + common_offset);
+        				square.setPosition((x * common_offset) + my_offset, (y * common_offset) + common_offset);
         			}
-        			
         			window.draw(square);
         		}
         	}
         }
-
 		// refresh part
 
 		window.display();
